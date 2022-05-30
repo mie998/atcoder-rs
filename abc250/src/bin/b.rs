00f7bb93968e9c51da6a3737e8dce7bd75f81518
mie998 <nishiwaki.kyoto@gmail.com>
2022-05-30 16:13:51 +0900

    abc250 b

--- abc250/src/bin/b.rs
+++ abc250/src/bin/b.rs
@@ -3,71 +3,22 @@ use proconio::input;
 const BLACK: char = '#';
 const WHITE: char = '.';
 fn main() {
-    input! {n: isize, a: isize, b: isize};
-    let mut original_tile = String::from("");
-    for _ in 0..a {
-        for _ in 0..b {
-            original_tile.push(WHITE);
-        }
-        original_tile.push('\n')
-    }
-    original_tile = original_tile.trim().to_string();
-
-    let mut ans_tile = String::from("");
-    for _ in 0..n {
-        for _ in 0..n {
-            let tile = black2white(&original_tile);
-            ans_tile = push_str_row(&ans_tile, &tile).to_string();
-        }
-        ans_tile.push('\n')
-    }
-    print!("{}", ans_tile);
-}
-
-fn black2white(s: &String) -> String {
-    let mut out = String::from("");
-    for c in s.chars() {
-        if c == BLACK {
-            out.push(WHITE);
-        } else {
-            out.push(c);
-        }
-    }
-    return out;
-}
-fn white2black(s: &String) -> String {
-    let mut out = String::from("");
-    for c in s.chars() {
-        if c == WHITE {
-            out.push(BLACK);
-        } else {
-            out.push(c);
-        }
-    }
-    return out;
-}
-
-fn push_str_row(s1: &str, s2: &str) -> String {
-    let mut split1 = s1.split('\n');
-    let mut split2 = s2.split('\n');
-    let mut result = String::from("");
-    loop {
-        match (split1.next(), split2.next()) {
-            (Some(sp1), Some(sp2)) => {
-                result.push_str(sp1);
-                result.push_str(sp2);
-                result.push('\n');
+    input! {n: usize, a: usize, b: usize};
+    let mut board = vec![vec![WHITE; n * b]; n * a];
+    for ni in 0..n {
+        for nj in 0..n {
+            let ch = if (ni + nj) % 2 == 1 { BLACK } else { WHITE };
+            for i in (ni * b)..(ni * b + b) {
+                for j in (nj * a)..(nj * a + a) {
+                    board[j][i] = ch;
+                }
             }
-            (Some(_), None) => {
-                println!("there is remaining chars in parameter1");
-                break;
-            }
-            (None, Some(_)) => {
-                println!("there is remaining chars in parameter2");
-                break;
-            }
-            (None, None) => break,
         }
     }
-    return result;
+    let ans = board
+        .iter()
+        .map(|vc| vc.iter().collect::<String>())
+        .collect::<Vec<String>>()
+        .join("\n");
+    println!("{}", ans);
 }
