24fe9adc4e544b4b5d77169627175f3be1879345
mie998 <nishiwaki.kyoto@gmail.com>
2023-05-07 22:38:20 +0900

    typical 004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,3 +1,22 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        h: usize,
+        w: usize,
+        a: [[i32; w]; h],
+    }
+    let mut rows = vec![0; h];
+    let mut columns = vec![0; w];
+    for i in 0..h {
+        for j in 0..w {
+            rows[i] += a[i][j];
+            columns[j] += a[i][j];
+        }
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            print!("{} ", rows[i] + columns[j] - a[i][j]);
+        }
+        println!();
+    }
 }
